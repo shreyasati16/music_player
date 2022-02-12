f0891ac11da534fde8b7f02a6d524c6528dd0566
shreyasati16 <85679706+shreyasati16@users.noreply.github.com>
2022-02-12 11:37:37 +0530

    Add files via upload

---
+++ js/myjQuery.js
@@ -0,0 +1,11 @@
+<script>
+    $(document).ready(function(){
+    $('#masterPlay').click(function(){
+        console.log('you clicked on play')
+        $('.loader').fadeIn()
+       
+
+    })
+   
+});
+</script>
